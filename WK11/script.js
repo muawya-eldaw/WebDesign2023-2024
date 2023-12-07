240474b0784c2b9634396e4bc1f20013da3a43cc
muawya-eldaw <muawya.eldaw@northampton.ac.uk>
2023-12-07 15:46:58 +0000

    Comments added to the JS code

--- WK11/script.js
+++ WK11/script.js
@@ -1,11 +1,21 @@
+// this function changes the content of the heading to 'new content'
 function myClickFunction() {
     var element = document.getElementById('pageheading'); 
     element.firstChild.nodeValue = 'New Heading';
 }
 
+//this function detects the clicking on the paragraph
+function myLoadFunction_2() {
+    var element = document.getElementById('pID'); 
+    element.addEventListener('click', myClickFunction);
+}
+
+/*
+//this function detects the clicking on the heading
 function myLoadFunction() {
     var element = document.getElementById('pageheading'); 
     element.addEventListener('click', myClickFunction);
 }
+*/
 
-document.addEventListener('DOMContentLoaded', myLoadFunction);
+document.addEventListener('DOMContentLoaded', myLoadFunction_2);
